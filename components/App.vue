7f95dc61cb71aafbfd1da6b3fb6f09464493ad9a
leo6liu <leo.battalora@gmail.com>
2021-08-25 23:41:52 -0400

    ffmpeg not loading

---
+++ components/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>hello from the App component</p>
+    <p>ffmpeg loaded: {{ ready }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
+const ffmpeg = createFFmpeg({ log: true });
+
+export default {
+  data() {
+    return {
+      ready: false,
+    };
+  },
+  methods: {
+    async load() {
+      console.log('start loading ffmpeg...');
+      await ffmpeg.load();
+      console.log('finished loading ffmpeg...');
+      this.ready = true;
+    },
+  },
+  created() {
+    this.load();
+  },
+};
+</script>
+
+<style>
+</style>
